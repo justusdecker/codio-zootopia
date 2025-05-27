72daeb30027e0ab0ae0d4bc6b8db1733a2244612
Justus <justus.d2025@gmail.com>
2025-05-27 18:25:49 +0200

    access data from json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,1 +1,12 @@
 from modules.load_data import load_data
+
+animals = load_data('animals_data.json')
+
+if __name__ == '__main__':
+    for animal in animals:
+
+        name = animal["name"]
+        diet = animal["characteristics"].get('diet',None)
+        location = animal["locations"][0]
+        ttype = animal["characteristics"].get('type',None)
+        print(f'{name}\n{diet}\n{location}{"\n" + ttype if ttype is not None else ""}\n')
