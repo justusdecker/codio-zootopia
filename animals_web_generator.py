8d1d85e1abc275614de3a25b3ef8ca8b375030ce
Justus <justus.d2025@gmail.com>
2025-05-27 18:39:06 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,7 +2,22 @@ from modules.load_data import load_data
 
 animals = load_data('animals_data.json')
 
+def load_and_rewrite_html(path_input:str, path_output: str) -> None:
+    with open(path_input,'r') as file_in:
+        html = file_in.read()
+    html = html.replace('__REPLACE_ANIMALS_INFO__',generate_unsorted_list())
+    with open(path_output,'w') as file_out:
+        file_out.write(html)
+def generate_unsorted_list():
+    html_string = ''
+    for animal in animals:
+        type_failsave = animal["characteristics"].get('type',None)
+        html_string += f"{animal['name']}\n{animal['characteristics']['diet']}\n{animal['locations'][0]}\n{"\n" + type_failsave if type_failsave is not None else ""}\n"
+    return html_string
 if __name__ == '__main__':
+    
+    load_and_rewrite_html('animals_template.html','animals.html')
+    
     for animal in animals:
 
         name = animal["name"]
