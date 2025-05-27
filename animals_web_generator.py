03ad20102a66d627a326aa35d5f8361b21c0b4c8
Justus <justus.d2025@gmail.com>
2025-05-27 18:55:36 +0200

    organized: single animal serialization

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,7 +2,13 @@ from modules.load_data import load_data
 
 animals = load_data('animals_data.json')
 
-def load_and_rewrite_html(path_input:str, path_output: str) -> None:
+def serialize_animal(animal):
+    type_failsave = animal["characteristics"].get('type',None)
+    nam, die, loc = animal['name'], animal['characteristics']['diet'], animal['locations'][0], 
+    typ_ext = "\n<strong>Type: </strong>\n" + type_failsave + '<br>' if type_failsave is not None else ""
+    return f'<li class="cards__item"><div class="card__title">{nam}</div>\n<p class="card__text">\n<strong>Diet: </strong>{die}<br>\n<strong>Location: </strong>{loc}<br>{typ_ext}\n</p>\n</li>\n'
+
+def serialize_html(path_input:str, path_output: str) -> None:
     with open(path_input,'r') as file_in:
         html = file_in.read()
     html = html.replace('__REPLACE_ANIMALS_INFO__',generate_unsorted_list())
@@ -12,15 +18,13 @@ def load_and_rewrite_html(path_input:str, path_output: str) -> None:
 def generate_unsorted_list():
     html_string = ''
     for animal in animals:
-        type_failsave = animal["characteristics"].get('type',None)
-        nam, die, loc = animal['name'], animal['characteristics']['diet'], animal['locations'][0], 
-        typ_ext = "\n<strong>Type: </strong>\n" + type_failsave + '<br>' if type_failsave is not None else ""
-        html_string += f'<li class="cards__item"><div class="card__title">{nam}</div>\n<p class="card__text">\n<strong>Diet: </strong>{die}<br>\n<strong>Location: </strong>{loc}<br>{typ_ext}\n</p>\n</li>\n'
+        html_string += serialize_animal(animal)
+
     return html_string
 
 if __name__ == '__main__':
     
-    load_and_rewrite_html('animals_template.html','animals.html')
+    serialize_html('animals_template.html','animals.html')
     
     for animal in animals:
 
